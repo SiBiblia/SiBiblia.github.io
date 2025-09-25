a9b711bc7782a62eaa4ff4347b1347a85513850c
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2025-09-25 16:22:44 -0500

    c3. coding pdf generation

--- code/bq_pdf_mgr.js
+++ code/bq_pdf_mgr.js
@@ -7,17 +7,68 @@
 //import * as MOD_PDF from "https://cdnjs.cloudflare.com/ajax/libs/jspdf/3.0.3/jspdf.umd.min.js";
 
 export function test_pdf(){
-	const doc = new window.jspdf.jsPDF();
+	const doc = new window.jspdf.jsPDF({
+		orientation: 'p',
+		unit: 'mm',
+		format: 'letter',
+		putOnlyUsedFonts: true
+	});
+	
+	const fl = doc.getFontList();
+	console.log("getFontList= " + JSON.stringify(fl, null, " "));
 
-	doc.setFontSize(40);
-	doc.text("Octonyan loves jsPDF", 35, 25);
 	const img_cod = get_code_img();
-	if(img_cod != null){
-		doc.addImage(img_cod, "PNG", 15, 40, 180, 180);	
-	} else {
-		doc.addImage("../img/trophy.webp", "WEBP", 15, 40, 180, 180);	
+	//const id = "/?u=JoseLuisQuiroga";
+	const id = "/?u=01234567890123456789";
+	
+	const txt = `Splits a given string into an array of strings. Uses 'size' value (in measurement units declared as default for the jsPDF instance) and the font's "widths" and "Kerning" tables, where available, to determine display length of a given string for a given font.`;
+	
+	const xx1 = 8;
+	const xx2 = 110;
+	const yy = 10;
+	const hh = 50;
+	const lines = 5;
+	
+	let ii = 0;
+	for(ii = 0; ii < lines; ii++){
+		const yy1 = yy + (hh * ii);
+		add_side_1(doc, xx1, yy1, id, img_cod);
+		add_side_1(doc, xx2, yy1, id, img_cod);
 	}
+
+	doc.addPage();	
+
+	for(ii = 0; ii < lines; ii++){
+		const yy1 = yy + (hh * ii);
+		add_side_2(doc, xx1, yy1, txt);
+		add_side_2(doc, xx2, yy1, txt);
+	}
+	
 	doc.save("image_jspdf.pdf");	
+}
+
+function add_side_1(doc, xx, yy, id, img_cod){
+	const mx = 4;
+	const my = 5;
+	doc.setFontSize(20);
+	doc.setFont("Times", "bold");
+	doc.text("SiBiblia.com", xx + mx, yy + my + 15);
+	doc.setFontSize(12);
+	doc.text(id, xx + mx, yy + my + 20);
+	if(img_cod != null){
+		doc.addImage(img_cod, "PNG", xx + mx + 55, yy + my, 35, 35);	
+	}
+	doc.rect(xx, yy, 97, 45);
+}
+
+function add_side_2(doc, xx, yy, txt){
+	const arr = doc.splitTextToSize(txt, 89);
+	const mx = 4;
+	const my = 5;
+	doc.setFontSize(12);
+	//doc.setFont("Times", "bold");
+	doc.text(arr, xx + mx, yy + my + 5);
+	doc.rect(xx, yy, 97, 45);
 }
 
 function get_code_img(){
@@ -38,4 +89,56 @@ function get_code_img(){
 	return the_img;
 }
 
+/*
+getFontList= {
+ "helvetica": [
+  "normal",
+  "bold",
+  "italic",
+  "bolditalic"
+ ],
+ "Helvetica": [
+  "",
+  "Bold",
+  "Oblique",
+  "BoldOblique"
+ ],
+ "courier": [
+  "normal",
+  "bold",
+  "italic",
+  "bolditalic"
+ ],
+ "Courier": [
+  "",
+  "Bold",
+  "Oblique",
+  "BoldOblique"
+ ],
+ "times": [
+  "normal",
+  "bold",
+  "italic",
+  "bolditalic"
+ ],
+ "Times": [
+  "Roman",
+  "Bold",
+  "Italic",
+  "BoldItalic"
+ ],
+ "zapfdingbats": [
+  "normal"
+ ],
+ "ZapfDingbats": [
+  ""
+ ],
+ "symbol": [
+  "normal"
+ ],
+ "Symbol": [
+  ""
+ ]
+} bq_pdf_mgr.js:18:10
 
+*/
